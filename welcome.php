27defbdb0445844e2b766501edeb4e37b73dfb87
Nada Ali Nour Eldeen <48387157+NadaAlinour@users.noreply.github.com>
2023-01-15 22:50:03 +0200

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,7 +4,7 @@ session_start();
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "registration";
+$dbname = "registration2";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
